f34c19ac3401f913f3cad8cc2421ef8082ecdb70
Saiful-Islam45 <saiful.islam.ice.45@gmail.com>
2022-10-04 07:18:22 +0600

    why us

--- src/components/WhyUs/ChooseItem.js
+++ src/components/WhyUs/ChooseItem.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import './ChooseItem.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 const ChooseItem = ({ item }) => {
-    const { title, description, img, icon } = item;
-    return (
-        <div className="col-xl-4">
-            <div className="single-item pb-3">
-                <div className="card">
-                    <img className="card-img-top" src={img} alt="" />
-                    <div className="card-body choose-aria-content d-flex">
-                        <div className="choose-aria-icon">
-                            <img src={icon} alt="" />
-                        </div>
-                        <div>
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <Link style={{ textDecoration: 'none' }} to={"itemDetails/" + 10} >
-                            <h6>See more <FontAwesomeIcon icon={faArrowRight} /></h6>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	const { title, description, img, icon } = item;
+	return (
+		<div className="col-xl-4">
+			<div className="single-item pb-3">
+				<div className="card">
+					<img className="card-img-top" src={img} alt="" />
+					<div className="card-body choose-aria-content d-flex">
+						<div className="choose-aria-icon">
+							<img src={icon} alt="" />
+						</div>
+						<div>
+							<h5 className="card-title">{title}</h5>
+							<p className="card-text">{description}</p>
+							<Link style={{ textDecoration: 'none' }} to={'itemDetails/' + 10}>
+								<h6>
+									See more <FontAwesomeIcon icon={faArrowRight} />
+								</h6>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ChooseItem;
