7b94cde5b568ff6e1924c9a880d0cde94c0481ae
Saiful-Islam45 <saiful.islam.ice.45@gmail.com>
2022-10-04 08:38:22 +0600

    header responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,16 +9,25 @@ import { Link } from 'react-router-dom';
 const Header = (props) => {
 	const auth = useAuth();
 	return (
-		<div>
-			<div className="container-fluid" style={{position: 'fixed',backgroundColor: 'white', zIndex: 20, boxShadow:' 3px 3px 5px #edf2ef'}}>
-				<nav className="navbar navbar-expand-lg navbar-light">
-					<a className="navbar-brand" href="/">
-						<img src={logo} alt="" />
-					</a>
-					<ul className="nav justify-content-end" id="navbarTogglerDemo03">
+		<nav className="navbar navbar-light navbar-expand-md bg-faded justify-content-center">
+			<div className="container">
+				<a href="/" className="navbar-brand d-flex w-50 me-auto">
+					<img src={logo} alt="" />
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#collapsingNavbar3"
+				>
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div className="navbar-collapse collapse w-100" id="collapsingNavbar3">
+					<ul className="nav navbar-nav ms-auto w-100 justify-content-end">
 						<li className="nav-item">
 							<a className="nav-link" href="/checkout">
-								<FontAwesomeIcon icon={faShoppingCart} /> <span className="ml-2">{props.count}</span>
+								<FontAwesomeIcon icon={faShoppingCart} />{' '}
+								<span className="ml-2">{props.count}</span>
 							</a>
 						</li>
 						<li className="nav-item">
@@ -32,7 +41,6 @@ const Header = (props) => {
 								</Link>
 							)}
 						</li>
-
 						<li className="nav-item">
 							{auth.user ? (
 								<a className="nav-link color-pink" href="/login">
@@ -52,9 +60,55 @@ const Header = (props) => {
 							)}
 						</li>
 					</ul>
-				</nav>
+				</div>
 			</div>
-		</div>
+		</nav>
+		// <div>
+		// 	<div className="container-fluid" style={{position: 'fixed',backgroundColor: 'white', zIndex: 20, boxShadow:' 3px 3px 5px #edf2ef'}}>
+		// 		<nav className="navbar navbar-expand-lg navbar-light">
+		// 			<a className="navbar-brand" href="/">
+		// 				<img src={logo} alt="" />
+		// 			</a>
+		// 			<ul className="nav justify-content-end" id="navbarTogglerDemo03">
+		// 				<li className="nav-item">
+		// 					<a className="nav-link" href="/checkout">
+		// 						<FontAwesomeIcon icon={faShoppingCart} /> <span className="ml-2">{props.count}</span>
+		// 					</a>
+		// 				</li>
+		// 				<li className="nav-item">
+		// 					{auth.user ? (
+		// 						<Link to="/checkout" className="nav-link">
+		// 							{auth.user.displayName}
+		// 						</Link>
+		// 					) : (
+		// 						<Link to="/login" className="nav-link">
+		// 							Login
+		// 						</Link>
+		// 					)}
+		// 				</li>
+
+		// 				<li className="nav-item">
+		// 					{auth.user ? (
+		// 						<a className="nav-link color-pink" href="/login">
+		// 							<button
+		// 								onClick={() => {
+		// 									auth.signOut();
+		// 								}}
+		// 								className="btn btn-rounded"
+		// 							>
+		// 								Sign Out
+		// 							</button>
+		// 						</a>
+		// 					) : (
+		// 						<a href="/login" className="nav-link color-pink">
+		// 							<button className="btn btn-rounded">Sign Up</button>
+		// 						</a>
+		// 					)}
+		// 				</li>
+		// 			</ul>
+		// 		</nav>
+		// 	</div>
+		// </div>
 	);
 };
 
