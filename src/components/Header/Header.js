f04f1c14b5e0b309572823a3f932914c45f15eb3
Saiful-Islam45 <saiful.islam.ice.45@gmail.com>
2022-10-04 08:39:16 +0600

    code refactor

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,8 +26,7 @@ const Header = (props) => {
 					<ul className="nav navbar-nav ms-auto w-100 justify-content-end">
 						<li className="nav-item">
 							<a className="nav-link" href="/checkout">
-								<FontAwesomeIcon icon={faShoppingCart} />{' '}
-								<span className="ml-2">{props.count}</span>
+								<FontAwesomeIcon icon={faShoppingCart} /> <span className="ml-2">{props.count}</span>
 							</a>
 						</li>
 						<li className="nav-item">
@@ -63,52 +62,6 @@ const Header = (props) => {
 				</div>
 			</div>
 		</nav>
-		// <div>
-		// 	<div className="container-fluid" style={{position: 'fixed',backgroundColor: 'white', zIndex: 20, boxShadow:' 3px 3px 5px #edf2ef'}}>
-		// 		<nav className="navbar navbar-expand-lg navbar-light">
-		// 			<a className="navbar-brand" href="/">
-		// 				<img src={logo} alt="" />
-		// 			</a>
-		// 			<ul className="nav justify-content-end" id="navbarTogglerDemo03">
-		// 				<li className="nav-item">
-		// 					<a className="nav-link" href="/checkout">
-		// 						<FontAwesomeIcon icon={faShoppingCart} /> <span className="ml-2">{props.count}</span>
-		// 					</a>
-		// 				</li>
-		// 				<li className="nav-item">
-		// 					{auth.user ? (
-		// 						<Link to="/checkout" className="nav-link">
-		// 							{auth.user.displayName}
-		// 						</Link>
-		// 					) : (
-		// 						<Link to="/login" className="nav-link">
-		// 							Login
-		// 						</Link>
-		// 					)}
-		// 				</li>
-
-		// 				<li className="nav-item">
-		// 					{auth.user ? (
-		// 						<a className="nav-link color-pink" href="/login">
-		// 							<button
-		// 								onClick={() => {
-		// 									auth.signOut();
-		// 								}}
-		// 								className="btn btn-rounded"
-		// 							>
-		// 								Sign Out
-		// 							</button>
-		// 						</a>
-		// 					) : (
-		// 						<a href="/login" className="nav-link color-pink">
-		// 							<button className="btn btn-rounded">Sign Up</button>
-		// 						</a>
-		// 					)}
-		// 				</li>
-		// 			</ul>
-		// 		</nav>
-		// 	</div>
-		// </div>
 	);
 };
 
